58fbc0077f579d40e0595e1508e649cfb6bbede0
mayoi_Akira <akane36@163.com>
2026-03-20 10:56:07 +0800

    create

---
+++ src/main/java/com/bot/akane/AkaneApplication.java
@@ -0,0 +1,13 @@
+package com.bot.akane;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkaneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AkaneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bot/akane/AkaneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bot.akane;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AkaneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
